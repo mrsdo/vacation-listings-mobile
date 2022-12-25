8d8f5fc4cefcbd403506a344936c2ac72c16a9a7
Marti D <marti.dolce@29signals.org>
2022-12-25 09:50:50 -0500

    Added HomePage template with Nav
    Next: Add Home Page Content per site: https://rdolcegroup.com

---
+++ pages/HomePage.js
@@ -0,0 +1,18 @@
+/*
+ * HomeScreen.js | M.Dolce, React Native Portfolio, marti.dolce@29signals.org, 202212
+ * Function ---
+ * Default Home page.
+ * ------------
+ */
+
+import { Text, View } from 'react-native';
+
+const HomePage = () => {
+    return (
+        <View>
+            <Text>[Home Screen]</Text>
+        </View>
+    );
+};
+
+export default HomePage;
